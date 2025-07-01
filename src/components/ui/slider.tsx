17c7a145d91f0835d67498ab54e9c61464f594fc
Sagar Vadalia <sv344@cornell.edu>
2025-07-01 11:47:28 -0400

    adjust imports

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
 
-import { cn } from "../lib/utils.ts";
+import { cn } from "../utils";
 
 function Slider({
   className,
