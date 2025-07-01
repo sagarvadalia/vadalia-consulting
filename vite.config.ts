783c160bbda0a8e07c39e3283afea4995c4f01fa
Sagar Vadalia <sv344@cornell.edu>
2025-07-01 11:41:51 -0400

    dirname

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
+import path, { resolve } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
